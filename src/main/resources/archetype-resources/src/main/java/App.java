db1b182b0b306b486ab4fa771eb32655138ad808
erwan <erwan.guillet44@hotmail.fr>
2018-04-10 15:34:21 +0200

    init repository

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $my_finance.my_finance_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
